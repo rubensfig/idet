be62de09ac49333ac6eda72ee354c7aec662c4a6
rubensfig <r.figueiredo.52@gmail.com>
2016-12-28 22:57:22 +0000

    final?

--- idet.py
+++ idet.py
@@ -3,11 +3,23 @@
 from sniffer import Sniffer
 import os
 import argparse
+import sys;
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def initialPrint():
     os.system("toilet -f mono12  idet")
 
-def writeToFile(timestamp, ipsrc, len, layer):
+def writeToFile(timestamp, ipsrc, ipdest, lens, layer):
     f = ""
     try:
         f = open('cap.txt', 'a')
@@ -15,19 +27,60 @@ def writeToFile(timestamp, ipsrc, len, layer):
         print "Unexpected error:", sys.exc_info()[0]
         raise
 
-    string = "# %s: %s, %s -> %s\n" % (timestamp, ipsrc, len, layer)
+    string = "# %s: %s - %s, %s -> %s\n" % (timestamp, ipsrc, ipdest, lens, layer)
+    print string
     f.write(string)
     f.close()
 
 def analise():
-    print "analysing"
+    try :
+        f = open('cap.txt', 'r');
+    except:
+        raise
+    lt = [];
+
+    ipsrc = '192.168.109.140'
+    #print ipsrc
+    protocolsearchParam = 'SSL'
+    sizesearchParam = 1400
+    sqlisearchParam = 700
+    offset = 200
+    setFlag = 0
+
+    for line in f:
+        lt.append(line);
+    for i in lt:
+        pkt = i;
+        if setFlag == 1:
+            if i.find(protocolsearchParam) != -1:
+                i = i.split(',')[1]
+                i = i.split()[0]
+                if int(i) > sqlisearchParam and int(i) <= (sqlisearchParam + offset):
+                        print bcolors.WARNING +  "Probable SQLI on packet" + " " + pkt + bcolors.ENDC
+            setFlag = 0
+            continue
+
+        if i.find(protocolsearchParam) != -1:
+            i = i.split(',')[1]
+            i = i.split()[0]
+            if int(i) > sizesearchParam and int(i) <= (sizesearchParam + offset):
+                setFlag = 1
+                continue
+    f.close()
+    f = open('cap.txt', 'w')
+    f.close()
 
 
 def theCap(count):
     ret = oj.prints(count)
     #writeToFile("12", "127.0.0.1", "10")
+    print ("Analysing")
     for pkt in ret:
-        writeToFile(pkt.sniff_timestamp, pkt.ip.src, pkt.length, pkt.highest_layer)
+        try:
+            writeToFile(pkt.sniff_timestamp, pkt.ip.src, pkt.ip.dst, pkt.length, pkt.highest_layer)
+        except AttributeError:
+            print AttributeError
+            continue
 
 def defParser():
     blalal = 0
@@ -36,7 +89,7 @@ def defParser():
     parser.add_argument('-a', dest="blalal", nargs="?", default = 0, help=' Analyse the cap.txt file')
 
     args = parser.parse_args()
-    #print args.blalal
+    #print args 1.blalal
     #print args.count
     if args.count == 0:
         raise Exception('Packet Value Capture not set.')
@@ -47,9 +100,15 @@ def defParser():
         return args.count
 if __name__ == "__main__":
     oj = Sniffer("wlan0")
-    initialPrint()
-    count = defParser()
+    #initialPrint()
+    #count = defParser()
+    '''
     if count == 0:
         analise()
     else:
         theCap(count)
+    '''
+    while True:
+        print "Analysing"
+        theCap(30)
+        analise()
